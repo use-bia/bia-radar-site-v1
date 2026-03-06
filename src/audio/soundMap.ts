602ff21bf4c9310eaa9b8f8fbb5fae529198fa65
Caracol <rafaelfariasm@live.com>
2026-03-06 17:36:00 -0300

    added english localization and new dub audios

--- src/audio/soundMap.ts
+++ src/audio/soundMap.ts
@@ -1,6 +1,6 @@
 export const SOUND_MAP = {
-	bia_introduction_portuguese: "/sounds/dublagem_oi_bia.mp3",
-	bia_introduction_english: "/sounds/drop_003.m4a",
+	bia_introduction_portuguese: "/sounds/dublagem_oi_bia.m4a",
+	bia_introduction_english: "/sounds/dub_hi_bia_english.m4a",
 	toggle_sound: "/sounds/drop_003.m4a",
 } as const;
 
